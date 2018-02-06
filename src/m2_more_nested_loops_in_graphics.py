fc6b92cacc9b4ff43d591a7374ad7d43849f0baf
tomlingb <tomlingb@rose-hulman.edu>
2018-02-05 20:04:21 -0500

    working on it

--- src/m2_more_nested_loops_in_graphics.py
+++ src/m2_more_nested_loops_in_graphics.py
@@ -3,8 +3,8 @@ This project demonstrates NESTED LOOPS (i.e., loops within loops)
 in the context of TWO-DIMENSIONAL GRAPHICS.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Geoffrey Tomlinson.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -48,10 +48,18 @@ def draw_upside_down_wall(rectangle, n, window):
       :type window: rg.RoseWindow
     and n is nonnegative.
     """
-    # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
-    #     Some tests are already written for you (above).
-    # ------------------------------------------------------------------
+
+    for j in range(n):
+        for k in range(n - j):
+            newrect = rectangle
+            newrect.attach_to(window)
+            window.render()
+
+
+            # ------------------------------------------------------------------
+            # TODO: 2. Implement and test this function.
+            #     Some tests are already written for you (above).
+            # ------------------------------------------------------------------
 
 
 # ----------------------------------------------------------------------
